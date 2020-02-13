0dc256df1148a4472610fb55d4982db52aa0538d
Amanda Rush <mandyrush85@gmail.com>
2020-02-12 17:27:11 -0700

    get product by id & pass to detail page, create orders page

--- controllers/shop.js
+++ controllers/shop.js
@@ -20,10 +20,28 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      pageTitle: product.pageTitle,
+      path: '/products',
+      product: product
+    });
+  });
+}
+
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     pageTitle: 'Your Cart',
     path: '/cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    pageTitle: 'Your Orders',
+    path: '/orders'
   });
 };
 
