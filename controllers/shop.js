ebae0f3ea4fea79c1fb4e3e42ebb8728ec66d3f1
Amanda Rush <mandyrush85@gmail.com>
2020-02-13 12:41:37 -0700

    Add cart model and post request to add product to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -37,6 +38,14 @@ exports.getCart = (req, res, next) => {
     path: '/cart'
   });
 };
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  })
+  res.redirect('/cart');
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
