caef045e8eed859d98a8586e8d5198406dab5771
Amanda Rush <mandyrush85@gmail.com>
2020-02-18 12:10:25 -0700

    Create add & edit functionality for admin products, create cart with add & delete functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -33,11 +33,33 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    pageTitle: 'Your Cart',
-    path: '/cart'
-  });
+  Cart.getCart(cart => {
+    Product.fetchAll(products => {
+      const cartProducts = [];
+      for(product of products) {
+        const cartProductData = cart.products.find(
+          prod => prod.id === product.id
+        );
+        if(cartProductData) {
+          cartProducts.push({productData: product, qty: cartProductData.qty});
+        }
+      }
+      res.render('shop/cart', {
+        pageTitle: 'Your Cart',
+        path: '/cart',
+        products: cartProducts
+      });
+    })
+  })
 };
+
+exports.postDeleteCartProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect('/cart');
+  });
+}
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
